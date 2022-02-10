58a582c10129a715d6ffd2fb4c0120b4c23a4883
mihkostas <47123339+mihkostas@users.noreply.github.com>
2022-02-10 11:20:56 +0200

    Update pyarp.sh

--- pyarp.sh
+++ pyarp.sh
@@ -2,9 +2,9 @@
  
 if [ $1 = '-s' ]; then
 
-   killall pyarp.py -9
    echo 0 > /proc/sys/net/ipv4/ip_forward
-
+   killall pyarp.py -9
+   
 else
   echo 1 > /proc/sys/net/ipv4/ip_forward
   ./pyarp.py $1 $2 > /dev/null &
